ebc6459aa6221c1b89efb95a28b2f98782ec5588
Dime <liy_0924@qq.com>
2026-02-11 17:25:00 +0800

    feat: 添加关于页面

---
+++ InitProject.bat
@@ -0,0 +1,5 @@
+@echo off
+
+git submodule
+git pull
+git submodule update --init --recursive --remote
--- src/PaperConnect.Desktop/Program.cs
+++ src/PaperConnect.Desktop/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia;
 using System;
+using System.IO;
+using Round.SDK.Logger;
 
 namespace PaperConnect.Desktop;
 
@@ -9,8 +11,16 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        var consoleRedirector = new ConsoleRedirector(Path.Combine(Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RoundStudio",
+                "PaperConnect", "PaperConnect.Desktop", "PaperConnect.Logs"),
+            $"[BedrockBoot.Logger] {DateTime.Now.ToString("yyyy.MM.dd HHmmss.fff")}.log"));
+        
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
